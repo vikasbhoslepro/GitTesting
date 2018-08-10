785525c8d5e1e64a45a61e3dde2c57b79bc0eba5
Vikas Bhosle <vikasbhoslepro@gmail.com>
2018-08-10 22:41:23 +0530

    initial

---
+++ app/src/main/java/testing/gittesting/MainActivity.java
@@ -0,0 +1,18 @@
+package testing.gittesting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private String hello(){
+        return "hello mlml ";
+
+    }
+}
--- app/src/main/java/testing/gittesting/yaaaa.java
+++ app/src/main/java/testing/gittesting/yaaaa.java
@@ -5,4 +5,5 @@ package testing.gittesting;
  */
 
 public class yaaaa {
+    String dd;
 }
